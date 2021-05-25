99bc584500300519e04426e98e5bbf823524807f
Noon Pokaratsiri <noonsky.noon@gmail.com>
2021-05-25 21:59:03 +0700

    q1a implemented

--- code/ex3_convnet.py
+++ code/ex3_convnet.py
@@ -3,27 +3,29 @@ import torch.nn as nn
 import torchvision
 import torchvision.transforms as transforms
 import numpy as np
-
 import matplotlib.pyplot as plt
+
 
 def weights_init(m):
     if type(m) == nn.Linear:
         m.weight.data.normal_(0.0, 1e-3)
         m.bias.data.fill_(0.)
 
+
 def update_lr(optimizer, lr):
     for param_group in optimizer.param_groups:
         param_group['lr'] = lr
 
-#--------------------------------
-# Device configuration
-#--------------------------------
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-print('Using device: %s'%device)
 
-#--------------------------------
+# --------------------------------
+# Device configuration
+# --------------------------------
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print('Using device: %s' % device)
+
+# --------------------------------
 # Hyper-parameters
-#--------------------------------
+# --------------------------------
 input_size = 3
 num_classes = 10
 hidden_size = [128, 512, 512, 512, 512, 512]
@@ -31,16 +33,15 @@ num_epochs = 20
 batch_size = 200
 learning_rate = 2e-3
 learning_rate_decay = 0.95
-reg=0.001
-num_training= 49000
-num_validation =1000
+reg = 0.001
+num_training = 49000
+num_validation = 1000
 norm_layer = None
 print(hidden_size)
 
-
-#-------------------------------------------------
+# -------------------------------------------------
 # Load the CIFAR-10 dataset
-#-------------------------------------------------
+# -------------------------------------------------
 #################################################################################
 # TODO: Q3.a Chose the right data augmentation transforms with the right        #
 # hyper-parameters and put them in the data_aug_transforms variable             #
@@ -49,49 +50,49 @@ data_aug_transforms = []
 # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
 # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
-norm_transform = transforms.Compose(data_aug_transforms+[transforms.ToTensor(),
-                                     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-                                     ])
+norm_transform = transforms.Compose(data_aug_transforms + [transforms.ToTensor(),
+                                                           transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+                                                           ])
 test_transform = transforms.Compose([transforms.ToTensor(),
                                      transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
                                      ])
 cifar_dataset = torchvision.datasets.CIFAR10(root='datasets/',
-                                           train=True,
-                                           transform=norm_transform,
-                                           download=False)
+                                             train=True,
+                                             transform=norm_transform,
+                                             download=False)
 
 test_dataset = torchvision.datasets.CIFAR10(root='datasets/',
-                                          train=False,
-                                          transform=test_transform
-                                          )
-#-------------------------------------------------
+                                            train=False,
+                                            transform=test_transform
+                                            )
+# -------------------------------------------------
 # Prepare the training and validation splits
-#-------------------------------------------------
+# -------------------------------------------------
 mask = list(range(num_training))
 train_dataset = torch.utils.data.Subset(cifar_dataset, mask)
 mask = list(range(num_training, num_training + num_validation))
 val_dataset = torch.utils.data.Subset(cifar_dataset, mask)
 
-#-------------------------------------------------
+# -------------------------------------------------
 # Data loader
-#-------------------------------------------------
+# -------------------------------------------------
 train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
                                            batch_size=batch_size,
                                            shuffle=True)
 
 val_loader = torch.utils.data.DataLoader(dataset=val_dataset,
-                                           batch_size=batch_size,
-                                           shuffle=False)
+                                         batch_size=batch_size,
+                                         shuffle=False)
 
 test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
                                           batch_size=batch_size,
                                           shuffle=False)
 
 
-#-------------------------------------------------
+# -------------------------------------------------
 # Convolutional neural network (Q1.a and Q2.a)
 # Set norm_layer for different networks whether using batch normalization
-#-------------------------------------------------
+# -------------------------------------------------
 class ConvNet(nn.Module):
     def __init__(self, input_size, hidden_layers, num_classes, norm_layer=None):
         super(ConvNet, self).__init__()
@@ -104,7 +105,43 @@ class ConvNet(nn.Module):
         #################################################################################
         layers = []
         # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+        # input channels for the first conv block == input_size == 3 channels
+        self.input_size = input_size
 
+        # first 5 values of hidden_layers are number of filters or out_channels
+        for h_size in hidden_layers[:5]:
+            # each convolution block consists of: conv, maxpool, relu
+            # need to preserve spatial dimensionality
+            layers.append(nn.Conv2d(in_channels=self.input_size,
+                                    out_channels=h_size,
+                                    kernel_size=3,
+                                    stride=1,
+                                    padding=1))
+            # after aech maxpool, filter size dimension is halved
+            layers.append(nn.MaxPool2d(kernel_size=2,
+                                       stride=2))
+            layers.append(nn.ReLU())
+
+            # update the input_size for the next conv block to be == to the out_channels of the previous conv block
+            self.input_size = h_size
+
+        # flatten after 5 conv layers, nn.Flatten() default start_dim = 1 already, so no need to specify,
+        # assuming dim = 0 is the batch dimension
+        # the flattened dimension should be == 512*1*1 == 512 or the last value in hidden_layers list
+        layers.append(nn.Flatten())
+        layers.append(nn.Linear(in_features=hidden_layers[-1], out_features=num_classes))
+
+        self.conv_net = nn.Sequential(*layers)
+
+        # self.conv_net follows the schematic below per model's specification
+        """
+        self.conv1 = nn.Conv2d(in_channels=input_size, out_channels=128, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(in_channels=128, out_channels=512, kernel_size=3, stride=1, padding=1)
+        self.conv3 = nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1)
+        self.conv4 = nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1)
+        self.conv5 = nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, stride=1, padding=1)
+        self.fc_classifier = nn.Linear(in_features=512*1*1, out_features=num_classes)
+        """
 
         # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
@@ -113,16 +150,31 @@ class ConvNet(nn.Module):
         # TODO: Implement the forward pass computations                                 #
         #################################################################################
         # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
+        out = self.conv_net(x)
 
+        # the above function using nn.Sequential is the same as below if implemented with nn.Sequential
+        """
+        # 5 blocks of convolution layers
+        x = nn.ReLU(self.max_pool(self.conv1(x)))
+        x = nn.ReLU(self.max_pool(self.conv2(x)))
+        x = nn.ReLU(self.max_pool(self.conv3(x)))
+        x = nn.ReLU(self.max_pool(self.conv4(x)))
+        x = nn.ReLU(self.max_pool(self.conv5(x)))
 
+        # flatten x
+        x = torch.flatten(x, start_dim=1)
+
+        # classification layer
+        out = self.fc_classifier(x)
+        """
         # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
         return out
 
 
-#-------------------------------------------------
+# -------------------------------------------------
 # Calculate the model size (Q1.b)
 # if disp is true, print the model parameters, otherwise, only return the number of parameters.
-#-------------------------------------------------
+# -------------------------------------------------
 def PrintModelSize(model, disp=True):
     #################################################################################
     # TODO: Implement the function to count the number of trainable parameters in   #
@@ -131,15 +183,17 @@ def PrintModelSize(model, disp=True):
     #################################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-
+    model_sz = None
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
     return model_sz
 
-#-------------------------------------------------
+
+# -------------------------------------------------
 # Calculate the model size (Q1.c)
 # visualize the convolution filters of the first convolution layer of the input model
-#-------------------------------------------------
+# -------------------------------------------------
 def VisualizeFilter(model):
+    pass
     #################################################################################
     # TODO: Implement the functiont to visualize the weights in the first conv layer#
     # in the model. Visualize them as a single image fo stacked filters.            #
@@ -149,12 +203,13 @@ def VisualizeFilter(model):
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-#======================================================================================
+
+# ======================================================================================
 # Q1.a: Implementing convolutional neural net in PyTorch
-#======================================================================================
+# ======================================================================================
 # In this question we will implement a convolutional neural networks using the PyTorch
 # library.  Please complete the code for the ConvNet class evaluating the model
-#--------------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------------
 model = ConvNet(input_size, hidden_size, num_classes, norm_layer=norm_layer).to(device)
 # Q2.a - Initialize the model with correct batch norm layer
 
@@ -162,16 +217,15 @@ model.apply(weights_init)
 # Print the model
 print(model)
 # Print model size
-#======================================================================================
+# ======================================================================================
 # Q1.b: Implementing the function to count the number of trainable parameters in the model
-#======================================================================================
+# ======================================================================================
 PrintModelSize(model)
-#======================================================================================
+# ======================================================================================
 # Q1.a: Implementing the function to visualize the filters in the first conv layers.
 # Visualize the filters before training
-#======================================================================================
+# ======================================================================================
 VisualizeFilter(model)
-
 
 # Loss and optimizer
 criterion = nn.CrossEntropyLoss()
@@ -196,9 +250,9 @@ for epoch in range(num_epochs):
         loss.backward()
         optimizer.step()
 
-        if (i+1) % 100 == 0:
-            print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'
-                   .format(epoch+1, num_epochs, i+1, total_step, loss.item()))
+        if (i + 1) % 100 == 0:
+            print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'
+                  .format(epoch + 1, num_epochs, i + 1, total_step, loss.item()))
 
     # Code to update the lr
     lr *= learning_rate_decay
@@ -257,5 +311,3 @@ with torch.no_grad():
 VisualizeFilter(model)
 # Save the model checkpoint
 torch.save(model.state_dict(), 'model.ckpt')
-
-
--- code/ex3_pretrained.py
+++ code/ex3_pretrained.py
@@ -4,72 +4,76 @@ import torchvision
 from torchvision import models
 import torchvision.transforms as transforms
 
+
 def weights_init(m):
     if type(m) == nn.Linear:
         m.weight.data.normal_(0.0, 1e-3)
         m.bias.data.fill_(0.)
 
+
 def update_lr(optimizer, lr):
     for param_group in optimizer.param_groups:
         param_group['lr'] = lr
 
-#--------------------------------
-# Device configuration
-#--------------------------------
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-print('Using device: %s'%device)
 
-#--------------------------------
+# --------------------------------
+# Device configuration
+# --------------------------------
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print('Using device: %s' % device)
+
+# --------------------------------
 # Hyper-parameters
-#--------------------------------
+# --------------------------------
 input_size = 32 * 32 * 3
-layer_config= [512, 256]
+layer_config = [512, 256]
 num_classes = 10
 num_epochs = 30
 batch_size = 200
 learning_rate = 1e-3
 learning_rate_decay = 0.99
-reg=0#0.001
-num_training= 49000
-num_validation =1000
+reg = 0  # 0.001
+num_training = 49000
+num_validation = 1000
 fine_tune = True
-pretrained=True
+pretrained = True
 
-data_aug_transforms = [transforms.RandomHorizontalFlip(p=0.5)]#, transforms.RandomGrayscale(p=0.05)]
-#-------------------------------------------------
+data_aug_transforms = [transforms.RandomHorizontalFlip(p=0.5)]  # , transforms.RandomGrayscale(p=0.05)]
+# -------------------------------------------------
 # Load the CIFAR-10 dataset
-#-------------------------------------------------
+# -------------------------------------------------
 # Q1,
-norm_transform = transforms.Compose(data_aug_transforms+[transforms.ToTensor(),
-                                     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
-                                     ])
+norm_transform = transforms.Compose(data_aug_transforms + [transforms.ToTensor(),
+                                                           transforms.Normalize([0.485, 0.456, 0.406],
+                                                                                [0.229, 0.224, 0.225]),
+                                                           ])
 cifar_dataset = torchvision.datasets.CIFAR10(root='datasets/',
-                                           train=True,
-                                           transform=norm_transform,
-                                           download=False)
+                                             train=True,
+                                             transform=norm_transform,
+                                             download=False)
 
 test_dataset = torchvision.datasets.CIFAR10(root='datasets/',
-                                          train=False,
-                                          transform=norm_transform
-                                          )
-#-------------------------------------------------
+                                            train=False,
+                                            transform=norm_transform
+                                            )
+# -------------------------------------------------
 # Prepare the training and validation splits
-#-------------------------------------------------
+# -------------------------------------------------
 mask = list(range(num_training))
 train_dataset = torch.utils.data.Subset(cifar_dataset, mask)
 mask = list(range(num_training, num_training + num_validation))
 val_dataset = torch.utils.data.Subset(cifar_dataset, mask)
 
-#-------------------------------------------------
+# -------------------------------------------------
 # Data loader
-#-------------------------------------------------
+# -------------------------------------------------
 train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
                                            batch_size=batch_size,
                                            shuffle=True)
 
 val_loader = torch.utils.data.DataLoader(dataset=val_dataset,
-                                           batch_size=batch_size,
-                                           shuffle=False)
+                                         batch_size=batch_size,
+                                         shuffle=False)
 
 test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
                                           batch_size=batch_size,
@@ -80,6 +84,7 @@ def set_parameter_requires_grad(model, feature_extracting):
     if feature_extracting:
         for param in model.parameters():
             param.requires_grad = False
+
 
 class VggModel(nn.Module):
     def __init__(self, n_class, fine_tune, pretrained=True):
@@ -93,7 +98,6 @@ class VggModel(nn.Module):
         #################################################################################
         # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-
         # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
     def forward(self, x):
@@ -102,12 +106,12 @@ class VggModel(nn.Module):
         #################################################################################
         # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-
         # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
         return out
 
+
 # Initialize the model for this run
-model= VggModel(num_classes, fine_tune, pretrained)
+model = VggModel(num_classes, fine_tune, pretrained)
 
 # Print the model we just instantiated
 print(model)
@@ -121,14 +125,12 @@ if fine_tune:
     params_to_update = []
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 else:
     params_to_update = model.parameters()
-    for name,param in model.named_parameters():
+    for name, param in model.named_parameters():
         if param.requires_grad == True:
-            print("\t",name)
-
+            print("\t", name)
 
 model.to(device)
 
@@ -154,9 +156,9 @@ for epoch in range(num_epochs):
         loss.backward()
         optimizer.step()
 
-        if (i+1) % 100 == 0:
-            print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'
-                   .format(epoch+1, num_epochs, i+1, total_step, loss.item()))
+        if (i + 1) % 100 == 0:
+            print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'
+                  .format(epoch + 1, num_epochs, i + 1, total_step, loss.item()))
 
     # Code to update the lr
     lr *= learning_rate_decay
@@ -181,7 +183,6 @@ for epoch in range(num_epochs):
         # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
         # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
-
 
         print('Validataion accuracy is: {} %'.format(100 * correct / total))
 
@@ -212,4 +213,3 @@ with torch.no_grad():
 
 # Save the model checkpoint
 torch.save(model.state_dict(), 'model.ckpt')
-
